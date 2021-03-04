501261788a518a4c9a3cdf279128377f4925db55
ba1999 <andrej.boldt@outllok>
2021-03-04 15:49:00 +0100

    Reaktionsspiel

--- app/src/main/java/com/example/simpletouch/MainActivity.kt
+++ app/src/main/java/com/example/simpletouch/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.example.simpletouch
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Point
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
     private val Tag = "MainActivity"
 
     private lateinit var mGestDetector : GestureDetector
-    private val tvGesture : TextView by lazy { findViewById(R.id.tvGesture)}
+    private val tvTime: TextView by lazy { findViewById(R.id.text_time)}
     private val touchview : TouchView by lazy { findViewById(R.id.touchView)}
+
+    private var startTime : Long = 0
+    private var elapsedTime : Long = 0
+    private val MAXCLICKS = 10
+    private var clickCount = 0
+    val r = Random
+    val CIRCLERADIUS = 20
+    var pos = Point()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,32 +38,70 @@ class MainActivity : AppCompatActivity() {
 
         touchview.setBackgroundColor(Color.BLUE)
 
+        val display = windowManager.defaultDisplay
+        val width = display.width
+        val height = display.height
+
+        pos.x = width/2
+        pos.y = height/2
+        touchview.setCircle(pos.x, pos.y, CIRCLERADIUS)
+
         touchview.setOnTouchListener { v, e->
             mGestDetector.onTouchEvent(e)
         }
+
+
     }
 
     //eigenen GestureListener implementieren als inner class, um auf Variablen zugreifen zu können
     inner class MyGestureListener() : GestureDetector.SimpleOnGestureListener() {
         override fun onDown(e: MotionEvent?): Boolean {
-            touchview.setCircle(Math.round(e!!.x), Math.round(e.y), 20)
+            val x = e!!.x
+            val y = e.y
+
+            // Abbruch, wenn der Klick nicht im Kreis liegt
+            if (!inCircle(x.toInt(), y.toInt())) return false
+
+            clickCount++
+
+            // To Dos beim ersten Klick
+            if (clickCount == 1) {
+                startTime = SystemClock.elapsedRealtime()
+                tvTime.visibility = View.INVISIBLE
+            }
+
+            val w = touchview.width
+            val h = touchview.height
+
+            // To Dos beim letzten Klick
+            if (clickCount == MAXCLICKS) {
+                // Zeit stoppen und anzeigen
+                val elapsedTime = SystemClock.elapsedRealtime() - startTime
+                tvTime.visibility = View.VISIBLE
+                tvTime.text = getString(R.string.timeString, elapsedTime.toString())
+                // Game Reset
+                clickCount = 0
+                touchview.setCircle(w / 2, h / 2, CIRCLERADIUS)
+                touchview.invalidate()
+                return true
+            }
+
+            // neue Position
+            pos.x = CIRCLERADIUS + r.nextInt(w - 2 * CIRCLERADIUS)
+            pos.y = CIRCLERADIUS + r.nextInt(h - 2 * CIRCLERADIUS)
+
+            touchview.setCircle(pos.x, pos.y, CIRCLERADIUS)
             touchview.invalidate()
-            tvGesture.text = getString(R.string.position_down,
-                    e!!.x.toString(), e.y.toString())
             return true
         }
 
-        override fun onFling(e1: MotionEvent?, e2: MotionEvent?,
-                             velocityX: Float, velocityY: Float): Boolean {
-            tvGesture.text = getString(R.string.position_fling,
-                    e1!!.x.toString(), e1.y.toString(),
-                    e2!!.x.toString(), e2.y.toString())
-            return true
-        }
 
-        override fun onLongPress(e: MotionEvent?) {
-            tvGesture.text = getString(R.string.position_longPress,
-                    e!!.x.toString(), e.y.toString())
+        fun inCircle(x: Int, y: Int) : Boolean {
+            val distx = x - pos.x
+            val disty = y - pos.y
+
+            if (distx*distx + disty*disty < CIRCLERADIUS*CIRCLERADIUS) return true
+            return false
         }
     }
 }
